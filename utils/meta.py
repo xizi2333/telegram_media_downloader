d7362a85d0f41422ddeb2970bb5ddb41036075ba
xizi2333 <xizi2333@users.noreply.github.com>
2026-09-11 06:18:11 -0700

    Remove copyright metadata dependency

--- utils/meta.py
+++ utils/meta.py
@@ -3,7 +3,7 @@ import platform
 
 from rich.console import Console
 
-from . import __copyright__, __license__, __version__
+from . import __license__, __version__
 
 APP_VERSION = f"Telegram Media Downloader {__version__}"
 DEVICE_MODEL = f"{platform.python_implementation()} {platform.python_version()}"
@@ -15,9 +15,7 @@ def print_meta(logger):
     """Prints meta-data of the downloader script."""
     console = Console()
     # pylint: disable = C0301
-    console.log(
-        f"[bold]Telegram Media Downloader v{__version__}[/bold],\n[i]{__copyright__}[/i]"
-    )
+    console.log(f"[bold]Telegram Media Downloader v{__version__}[/bold]")
     console.log(f"Licensed under the terms of the {__license__}", end="\n\n")
     logger.info(f"Device: {DEVICE_MODEL} - {APP_VERSION}")
     logger.info(f"System: {SYSTEM_VERSION} ({LANG_CODE.upper()})")
